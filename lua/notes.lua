a0a595749d95a2b9d6b0f63c7a964d5639087646
Stuart P. Bentley <stuart@testtrack4.com>
2011-08-20 01:03:23 -0400

    added notes on current state of icons: changed work page generator to use

---
+++ lua/notes.lua
@@ -0,0 +1,1037 @@
+return {
+  medic = {
+    assist_capturer = [=[
+
+    ]=],
+    assist_demoman = [=[
+
+    ]=],
+    assist_heavy = [=[
+
+    ]=],
+    assist_heavy_long = [=[
+
+    ]=],
+    assist_medic = [=[
+
+    ]=],
+    assist_pyro = [=[
+
+    ]=],
+    assist_scout = [=[
+
+    ]=],
+    assist_soldier = [=[
+
+    ]=],
+    assist_vs_nemeses = [=[
+
+    ]=],
+    bonesaw_medics = [=[
+
+    ]=],
+    bonesaw_nomisses = [=[
+
+    ]=],
+    bonesaw_spy_callers = [=[
+
+    ]=],
+    charge_blocker = [=[
+
+    ]=],
+    charge_friends = [=[
+
+    ]=],
+    charge_juggle = [=[
+
+    ]=],
+    counter_charge = [=[
+
+    ]=],
+    extinguish_teammates = [=[
+
+    ]=],
+    freezecam_ragdoll = [=[
+
+    ]=],
+    heal_achiever = [=[
+
+    ]=],
+    heal_callers = [=[
+
+    ]=],
+    heal_engineer = [=[
+
+    ]=],
+    heal_grind = [=[
+
+    ]=],
+    heal_huge = [=[
+
+    ]=],
+    heal_large = [=[
+
+    ]=],
+    heal_under_fire = [=[
+
+    ]=],
+    invite_join_charge = [=[
+
+    ]=],
+    kill_healed_spy = [=[
+
+    ]=],
+    kill_while_charged = [=[
+
+    ]=],
+    rapid_charge = [=[
+
+    ]=],
+    save_falling_teammate = [=[
+
+    ]=],
+    save_teammate = [=[
+
+    ]=],
+    setup_charge = [=[
+
+    ]=],
+    simul_charge = [=[
+
+    ]=],
+    switch_to_medic = [=[
+
+    ]=],
+    syringe_scouts = [=[
+
+    ]=],
+    top_scoreboard = [=[
+
+    ]=],
+  },
+  pyro = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    burn_medicpair = [=[
+
+    ]=],
+    burn_medics_charged = [=[
+
+    ]=],
+    burn_rj_soldier = [=[
+
+    ]=],
+    burn_snipers_zoomed = [=[
+
+    ]=],
+    burn_spies_as_you = [=[
+
+    ]=],
+    burn_spy_taunt = [=[
+
+    ]=],
+    camp_position = [=[
+
+    ]=],
+    camp_teleporters = [=[
+
+    ]=],
+    damage_grind = [=[
+      [noalpha]
+    ]=],
+    defend_points = [=[
+
+    ]=],
+    destroy_buildings = [=[
+      [rerun] demoman/destroy_buildings_grind (Cry Some Moor!)
+    ]=],
+    dominate_leavesvr = [=[
+
+    ]=],
+    double_ko = [=[
+
+    ]=],
+    force_waterjump = [=[
+
+    ]=],
+    freezecam_taunts = [=[
+
+    ]=],
+    ignite_flaregun = [=[
+
+    ]=],
+    kill_axe_large = [=[
+
+    ]=],
+    kill_axe_small = [=[
+
+    ]=],
+    kill_carriers = [=[
+
+    ]=],
+    kill_from_behind = [=[
+
+    ]=],
+    kill_grind = [=[
+
+    ]=],
+    kill_grind_large = [=[
+
+    ]=],
+    kill_heavies = [=[
+
+    ]=],
+    kill_multiweapons = [=[
+
+    ]=],
+    kill_postdeath = [=[
+
+    ]=],
+    kill_spies = [=[
+
+    ]=],
+    kill_taunt = [=[
+
+    ]=],
+    kill_taunters = [=[
+      [noalpha]
+    ]=],
+    kill_teamwork = [=[
+
+    ]=],
+    kill_ubercharge = [=[
+
+    ]=],
+    kill_underwater = [=[
+
+    ]=],
+    reflect_crocket_kill = [=[
+
+    ]=],
+    reflect_projectiles = [=[
+
+    ]=],
+    reveal_spies = [=[
+
+    ]=],
+    simulburn_scouts = [=[
+
+    ]=],
+  },
+  heavy = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    assist_grind = [=[
+
+    ]=],
+    assist_heavy_grind = [=[
+
+    ]=],
+    assist_medic_large = [=[
+
+    ]=],
+    block_cart = [=[
+
+    ]=],
+    block_invuln_heavy = [=[
+
+    ]=],
+    clear_stickybombs = [=[
+
+    ]=],
+    damage_taken = [=[
+
+    ]=],
+    defend_control_point = [=[
+
+    ]=],
+    defend_medic = [=[
+
+    ]=],
+    earn_medic_domination = [=[
+
+    ]=],
+    eat_sandwiches = [=[
+
+    ]=],
+    fire_lots = [=[
+
+    ]=],
+    first_to_cap = [=[
+
+    ]=],
+    freezecam_taunt = [=[
+
+    ]=],
+    heal_medikits = [=[
+
+    ]=],
+    kill_capping_enemies = [=[
+
+    ]=],
+    kill_crit_punch = [=[
+
+    ]=],
+    kill_dominated = [=[
+
+    ]=],
+    kill_flag_carriers = [=[
+
+    ]=],
+    kill_heavies_gloves = [=[
+
+    ]=],
+    kill_medic_pair = [=[
+
+    ]=],
+    kill_midair_minigun = [=[
+
+    ]=],
+    kill_scouts = [=[
+
+    ]=],
+    kill_shotgun = [=[
+
+    ]=],
+    kill_taunt = [=[
+
+    ]=],
+    kill_underwater = [=[
+
+    ]=],
+    kill_while_spunup = [=[
+
+    ]=],
+    payload_cap_grind = [=[
+
+    ]=],
+    receive_uber_grind = [=[
+
+    ]=],
+    revenge_assist = [=[
+
+    ]=],
+    stand_near_dispenser = [=[
+
+    ]=],
+    survive_crocket = [=[
+
+    ]=],
+    take_multi_damage = [=[
+
+    ]=],
+    teleport_fast_kill = [=[
+
+    ]=],
+    uncover_spies = [=[
+
+    ]=],
+  },
+  scout = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    assist_medic = [=[
+
+    ]=],
+    block_caps = [=[
+
+    ]=],
+    cap_flag_without_attacking = [=[
+
+    ]=],
+    capture_last_point = [=[
+
+    ]=],
+    capture_three_points = [=[
+      [noalpha]
+    ]=],
+    carrier_kill_carrier = [=[
+
+    ]=],
+    destroy_buildings_being_built = [=[
+
+    ]=],
+    destroy_sentry_with_pistol = [=[
+
+    ]=],
+    destroy_teleporters = [=[
+
+    ]=],
+    dodge_damage = [=[
+
+    ]=],
+    double_jumps = [=[
+
+    ]=],
+    doublejump_kill = [=[
+
+    ]=],
+    fast_cap = [=[
+
+    ]=],
+    first_blood = [=[
+
+    ]=],
+    first_blood_kill = [=[
+
+    ]=],
+    flag_cap_grind = [=[
+
+    ]=],
+    iron_man_kills = [=[
+
+    ]=],
+    kill_charged_medics = [=[
+
+    ]=],
+    kill_from_behind = [=[
+
+    ]=],
+    kill_in_dodge_cooldown = [=[
+
+    ]=],
+    kill_stunned = [=[
+
+    ]=],
+    knock_into_train = [=[
+
+    ]=],
+    lifetime_kills = [=[
+
+    ]=],
+    long_distance_runner = [=[
+      [noalpha]
+    ]=],
+    max_stuns = [=[
+
+    ]=],
+    start_and_finish_cap = [=[
+      [noalpha]
+    ]=],
+    steal_sandwich = [=[
+
+    ]=],
+    stun_capping_enemies = [=[
+
+    ]=],
+    stun_into_train = [=[
+
+    ]=],
+    stun_scout_with_their_ball = [=[
+
+    ]=],
+    stun_uber_enemies = [=[
+
+    ]=],
+    survive_damage = [=[
+
+    ]=],
+    taunt_kill = [=[
+
+    ]=],
+    three_flagcaps = [=[
+
+    ]=],
+    well_early_kill = [=[
+
+    ]=],
+  },
+  sniper = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    bow_kill_flagcarrier = [=[
+
+    ]=],
+    bow_kill_medic_pair = [=[
+
+    ]=],
+    bow_kill_whiledead = [=[
+
+    ]=],
+    bow_pin_heavy = [=[
+
+    ]=],
+    bow_pincushion = [=[
+      [noalpha]
+    ]=],
+    capture_flag = [=[
+
+    ]=],
+    destroy_sentryguns = [=[
+
+    ]=],
+    dominate_sniper = [=[
+      [alpha] The Australias are spaced evenly, in contrast to the original raster.
+    ]=],
+    freezecam_hat = [=[
+
+    ]=],
+    freezecam_wave = [=[
+
+    ]=],
+    get_backstabbed = [=[
+
+    ]=],
+    headshot_demoman = [=[
+      [noalpha]
+    ]=],
+    headshot_post_invuln = [=[
+      [noalpha]
+    ]=],
+    headshot_snipers = [=[
+      [rerun] spy/ambassador_sniper_grind (Skullpluggery)
+    ]=],
+    jarate_assists = [=[
+      [noalpha]
+    ]=],
+    jarate_dominated = [=[
+
+    ]=],
+    jarate_extinguish = [=[
+      [noalpha]
+    ]=],
+    jarate_group = [=[
+
+    ]=],
+    jarate_kill_melee = [=[
+      [noalpha]
+    ]=],
+    jarate_medic_pair = [=[
+
+    ]=],
+    jarate_reveal_spy = [=[
+      [noalpha]
+    ]=],
+    kill_at_roundstart = [=[
+      [noalpha]
+    ]=],
+    kill_charged_medic = [=[
+      [noalpha]
+    ]=],
+    kill_failed_spy = [=[
+
+    ]=],
+    kill_grind = [=[
+
+    ]=],
+    kill_invis_spy = [=[
+
+    ]=],
+    kill_midair_scout = [=[
+
+    ]=],
+    kill_objectives = [=[
+
+    ]=],
+    kill_rjer = [=[
+
+    ]=],
+    kill_spies_melee = [=[
+
+    ]=],
+    kill_unscoped = [=[
+
+    ]=],
+    kill_weapons = [=[
+
+    ]=],
+    rifle_no_missing = [=[
+
+    ]=],
+    taunt_kill = [=[
+
+    ]=],
+    top_scoreboard_grind = [=[
+
+    ]=],
+  },
+  spy = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    ambassador_grind = [=[
+      [noalpha]
+    ]=],
+    ambassador_scout_grind = [=[
+
+    ]=],
+    ambassador_sniper_grind = [=[
+      [rerun] sniper/headshot_snipers (Self-destruct Sequence)
+    ]=],
+    backstab_capping_enemies = [=[
+
+    ]=],
+    backstab_disguise_target = [=[
+      The globe here is using strokes instead of a path.
+    ]=],
+    backstab_disguised_spy = [=[
+
+    ]=],
+    backstab_dominating_enemy = [=[
+
+    ]=],
+    backstab_enemy_switch_pyro = [=[
+
+    ]=],
+    backstab_engy_sap_building = [=[
+
+    ]=],
+    backstab_friends = [=[
+
+    ]=],
+    backstab_grind = [=[
+
+    ]=],
+    backstab_medic_charged = [=[
+
+    ]=],
+    backstab_medic_healing_you = [=[
+
+    ]=],
+    backstab_medic_pair = [=[
+
+    ]=],
+    backstab_quick_kills = [=[
+
+    ]=],
+    backstab_snipers = [=[
+
+    ]=],
+    break_shield_kill_sniper = [=[
+
+    ]=],
+    bump_cloaked_spy = [=[
+
+    ]=],
+    camp_position = [=[
+
+    ]=],
+    dominate_sniper = [=[
+
+    ]=],
+    fast_cap = [=[
+
+    ]=],
+    feign_death_kill = [=[
+
+    ]=],
+    freezecam_flick = [=[
+
+    ]=],
+    kill_cp_defenders = [=[
+
+    ]=],
+    kill_spy_with_knife = [=[
+
+    ]=],
+    kill_working_engy = [=[
+
+    ]=],
+    knife_kill_while_jarated = [=[
+
+    ]=],
+    medic_healing_kill_enemy = [=[
+      [noalpha]
+    ]=],
+    revenge_with_backstab = [=[
+      [noalpha]
+    ]=],
+    sap_building_backstab_engy = [=[
+
+    ]=],
+    sapper_grind = [=[
+
+    ]=],
+    sapper_teamwork = [=[
+
+    ]=],
+    survive_burning = [=[
+      [noalpha]
+    ]=],
+    taunt_kill = [=[
+      [noalpha]
+    ]=],
+  },
+  soldier = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+      This is the icon for Scout, not Soldier (baseballs on the balloons). This is a fault in the upstream zipball.
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    assist_medic_uber = [=[
+
+    ]=],
+    bounce_then_shotgun = [=[
+
+    ]=],
+    buff_friends = [=[
+      [noalpha]
+    ]=],
+    buff_teammates = [=[
+      [noalpha]
+    ]=],
+    crouch_rocket_jump = [=[
+
+    ]=],
+    defend_cap_thirty_times = [=[
+      [noalpha]
+    ]=],
+    defend_medic = [=[
+      [noalpha]
+    ]=],
+    destroy_sentry_out_of_range = [=[
+      [noalpha]
+    ]=],
+    destroy_stickies = [=[
+      [noalpha]
+    ]=],
+    duo_soldier_kills = [=[
+      [noalpha]
+    ]=],
+    equalizer_streak = [=[
+      [noalpha]
+    ]=],
+    freezecam_gibs = [=[
+      [noalpha]
+    ]=],
+    freezecam_taunt = [=[
+      [noalpha]
+    ]=],
+    gib_grind = [=[
+      [noalpha]
+    ]=],
+    kill_airborne_target_while_airborne = [=[
+      [noalpha]
+    ]=],
+    kill_airborne_with_direct_hit = [=[
+
+    ]=],
+    kill_defenseless = [=[
+      [noalpha]
+    ]=],
+    kill_demoman_grind = [=[
+      [noalpha]
+    ]=],
+    kill_engy = [=[
+
+    ]=],
+    kill_five_stunned = [=[
+      [noalpha]
+    ]=],
+    kill_group_with_crocket = [=[
+      [noalpha]
+    ]=],
+    kill_on_fire = [=[
+      [noalpha]
+    ]=],
+    kill_pyro = [=[
+      [noalpha]
+    ]=],
+    kill_sniper_while_dead = [=[
+      [noalpha]
+    ]=],
+    kill_spy_killer = [=[
+
+    ]=],
+    kill_taunt = [=[
+      [noalpha]
+    ]=],
+    kill_twenty_from_above = [=[
+
+    ]=],
+    kill_two_during_rocket_jump = [=[
+      [noalpha]
+    ]=],
+    kill_with_equalizer_while_hurt = [=[
+
+    ]=],
+    mvp = [=[
+      [noalpha]
+    ]=],
+    nemesis_shovel_kill = [=[
+      [noalpha]
+    ]=],
+    ride_the_cart = [=[
+
+    ]=],
+    rj_equalizer_kill = [=[
+
+    ]=],
+    shoot_mult_crits = [=[
+      [noalpha]
+    ]=],
+    three_dominations = [=[
+      [noalpha]
+    ]=],
+  },
+  demoman = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    air_burst_kills = [=[
+      [rerun] engineer/destroy_stickies (How the Pests was Gunned)
+    ]=],
+    bounce_and_kill = [=[
+
+    ]=],
+    charge_kill = [=[
+
+    ]=],
+    crit_sword_kill = [=[
+
+    ]=],
+    damage_grind = [=[
+
+    ]=],
+    decapitate_cloaked_spy = [=[
+
+    ]=],
+    decapitate_equalizer = [=[
+
+    ]=],
+    decapitate_nemesis = [=[
+
+    ]=],
+    decapitate_players = [=[
+
+    ]=],
+    decapitate_players_fast = [=[
+
+    ]=],
+    destroy_buildings_grind = [=[
+      [rerun] pyro/destroy_buildings (Arsonist)
+    ]=],
+    destroy_buildings_with_medic = [=[
+      [rerun] scout/destroy_buildings_being_built (If You Build It)
+    ]=],
+    destroy_x_stickybombs = [=[
+
+    ]=],
+    dominate_three_engineers = [=[
+
+    ]=],
+    duo_demoman_kills = [=[
+
+    ]=],
+    environmental_kill = [=[
+      [rerun] scout/stun_into_train (Foul Territory)
+    ]=],
+    freezecam_rump = [=[
+
+    ]=],
+    freezecam_smile = [=[
+
+    ]=],
+    kill3_with_detonation = [=[
+
+    ]=],
+    kill3_with_pipe_setups = [=[
+
+    ]=],
+    kill_building_direct_hit = [=[
+
+    ]=],
+    kill_engi_sentry_dispenser = [=[
+
+    ]=],
+    kill_player_after_tp = [=[
+
+    ]=],
+    kill_soldier_grind = [=[
+
+    ]=],
+    kill_two_during_stickyjump = [=[
+
+    ]=],
+    kill_x_capping_onedet = [=[
+
+    ]=],
+    kill_x_defending = [=[
+
+    ]=],
+    kill_x_heavies_fullhp_onedet = [=[
+
+    ]=],
+    kill_x_scouts_pyros = [=[
+
+    ]=],
+    kill_x_with_directpipe = [=[
+
+    ]=],
+    killxsappingspies = [=[
+      [rerun] engineer/destroy_sappers (Fistful of Sappers)
+    ]=],
+    melee_kill_while_stickyjumping = [=[
+
+    ]=],
+    stickjump_distance = [=[
+
+    ]=],
+    stickyjump_cap = [=[
+
+    ]=],
+    taunt_kill = [=[
+
+    ]=],
+  },
+  engineer = {
+    achieve_progress1 = [=[
+
+    ]=],
+    achieve_progress2 = [=[
+
+    ]=],
+    achieve_progress3 = [=[
+
+    ]=],
+    building_carry = [=[
+
+    ]=],
+    destroy_sappers = [=[
+      [rerun] demoman/killxsappingspies (Spynal Tap)
+    ]=],
+    destroy_stickies = [=[
+      [rerun] demoman/air_burst_kills (Something Stickied This Way Comes)
+    ]=],
+    dispenser_extinguish = [=[
+
+    ]=],
+    dispenser_heal_grind = [=[
+
+    ]=],
+    dispenser_heal_group = [=[
+
+    ]=],
+    freezecam_sentry = [=[
+
+    ]=],
+    freezecam_taunt = [=[
+
+    ]=],
+    heavy_assist = [=[
+
+    ]=],
+    help_build_structure = [=[
+
+    ]=],
+    help_manual_sentry = [=[
+
+    ]=],
+    kill_assist = [=[
+
+    ]=],
+    kill_disguised_spy = [=[
+
+    ]=],
+    kill_flag_carriers = [=[
+
+    ]=],
+    kill_sentry_with_sentry = [=[
+
+    ]=],
+    kill_snipers_sentry = [=[
+
+    ]=],
+    kill_spy_two_sappers = [=[
+
+    ]=],
+    manual_cloaked_spy_kill = [=[
+
+    ]=],
+    manual_sentry_absorb_dmg = [=[
+
+    ]=],
+    manual_sentry_kills_beyond_range = [=[
+
+    ]=],
+    move_sentry_get_kill = [=[
+
+    ]=],
+    remove_sapper_long_dist = [=[
+
+    ]=],
+    repair_sentry_w_medic = [=[
+
+    ]=],
+    repair_team_grind = [=[
+
+    ]=],
+    revenge_crit_life = [=[
+
+    ]=],
+    revenge_crit_sentry_killer = [=[
+
+    ]=],
+    sentry_avenges_you = [=[
+
+    ]=],
+    sentry_kill_caps = [=[
+
+    ]=],
+    sentry_kill_lifetime_grind = [=[
+
+    ]=],
+    shotgun_kill_prev_sentry_target = [=[
+
+    ]=],
+    tank_damage = [=[
+
+    ]=],
+    taunt_kill = [=[
+
+    ]=],
+    teleport_grind = [=[
+
+    ]=],
+    upgrade_buildings = [=[
+
+    ]=],
+    waste_metal_grind = [=[
+
+    ]=],
+  },
+}
--- lua/used_scripts/wiki/make_work_page.lua
+++ lua/used_scripts/wiki/make_work_page.lua
@@ -1,21 +1,29 @@
-local achievements = require "lua.achievements"
+local tf = require "lua.achievements"
+local notes = require "lua.notes"
 
 local classes = {"Medic", "Pyro", "Heavy", "Scout", "Sniper",
   "Spy", "Soldier", "Demoman", "Engineer"}
 
 for i=1, #classes do
-  print('<tr><th colspan="2">'..classes[i]..'</th><tr>')
-  local class_achs = achievements[string.lower(classes[i])]
+  print('##'..classes[i])
+  local class = string.lower(classes[i])
   local tokens = {}
-  for k, v in pairs(class_achs) do
-    tokens[#tokens+1] = {
-      token = k,
-      name = v.name}
+  for k, v in pairs(tf[class]) do
+    local note = notes[class][k]
+    if note then --quick skip of missing achievment icons
+      tokens[#tokens+1] = {
+        token = k,
+        name = v.name,
+        note = note}
+    end
   end
   table.sort(tokens, function(m,n) return m.token < n.token end)
+  print"<table>"
+  print"<tr><th>Filename</th><th>Achievement name</th><th>Remaining work</th></tr>"
   for j=1, #tokens do
     print((string.gsub(
-      '<tr title="$name"><td>$token</td>\n  <td></td>\n</tr>',
+      '  <tr>\n    <td>$token</td>\n    <td>$name</td>\n    <td>\n$note</td>\n  </tr>',
       "%$(%a*)",tokens[j])))
   end
+  print"</table>"
 end
